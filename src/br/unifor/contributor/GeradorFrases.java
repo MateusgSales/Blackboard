e5ffb725586c849372624c8462d576b139b0acf6
MateusgSales <mateusg.sales@gmail.com>
2016-09-06 00:59:01 -0300

    Resposta...

---
+++ src/br/unifor/contributor/GeradorFrases.java
@@ -0,0 +1,40 @@
+package br.unifor.contributor;
+import br.unifor.blackboard.Blackboard;
+import br.unifor.blackboard.Solucao;
+
+
+public class GeradorFrases implements Contributor {
+	
+	
+	private void updateBlackboard(Solucao novaSolucao) {	
+		Blackboard.getInstance().update(novaSolucao);
+	}
+	
+	private Solucao geraFrase(Solucao solucao) {
+		int i = 0;
+		
+		while(i < solucao.getPalavras().size()) {
+			String palavra = solucao.getPalavras().get(i);
+			solucao.getPalavras().remove(0);
+			solucao.setFrase(palavra+" ");
+			i++;
+		} ;
+		
+		return solucao;
+		
+	}
+	
+	@Override
+	public Integer execCondition() {		
+		return Blackboard.getInstance().inspect(this);
+	}
+	
+
+	@Override
+	public void execAction() {
+		Solucao solucaoAtual = Blackboard.getInstance().getSolucao();		
+		Solucao novaSolucao = this.geraFrase(solucaoAtual);		
+		this.updateBlackboard(novaSolucao);		
+	}
+
+}
